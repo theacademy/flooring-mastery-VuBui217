163e5d13407b25abf30470cc58ff58ed0e9aa270
VuBui217 <vubui217@gmail.com>
2026-05-06 22:58:06 -0400

    Implement model classes

---
+++ FlooringMastery/src/main/java/com/sg/flooringmastery/model/Product.java
@@ -0,0 +1,41 @@
+package com.sg.flooringmastery.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String productType;
+    private BigDecimal costPerSquareFoot;
+    private BigDecimal laborCostPerSquareFoot;
+
+    // Constructor
+    public Product(String productType, BigDecimal costPerSquareFoot, BigDecimal laborCostPerSquareFoot) {
+        this.productType = productType;
+        this.costPerSquareFoot = costPerSquareFoot;
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+
+    // Getter and Setters
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        this.costPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return laborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+}
---
+++ FlooringMastery/src/main/java/com/sg/flooringmastery/model/Tax.java
@@ -0,0 +1,41 @@
+package com.sg.flooringmastery.model;
+
+import java.math.BigDecimal;
+
+public class Tax {
+    private String stateAbbreviation;
+    private String stateName;
+    private BigDecimal taxRate;
+
+    // Constructor
+    public Tax(String stateAbbreviation, String stateName, BigDecimal taxRate) {
+        this.stateAbbreviation = stateAbbreviation;
+        this.stateName = stateName;
+        this.taxRate = taxRate;
+    }
+
+    // Getters and Setters
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+
+    public void setStateAbbreviation(String stateAbbreviation) {
+        this.stateAbbreviation = stateAbbreviation;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public BigDecimal getTaxRate() {
+        return this.taxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        this.taxRate = taxRate;
+    }
+}
