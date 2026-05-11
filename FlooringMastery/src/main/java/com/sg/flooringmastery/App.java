9c470ddc9a3b4900e5aa186243a5fce212476ca6
VuBui217 <vubui217@gmail.com>
2026-05-10 23:05:41 -0400

    Implement Spring DI

--- FlooringMastery/src/main/java/com/sg/flooringmastery/App.java
+++ FlooringMastery/src/main/java/com/sg/flooringmastery/App.java
@@ -7,16 +7,22 @@ import com.sg.flooringmastery.service.FlooringMasteryServiceLayerImpl;
 import com.sg.flooringmastery.ui.FlooringMasteryView;
 import com.sg.flooringmastery.ui.UserIO;
 import com.sg.flooringmastery.ui.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        UserIO myIo = new UserIOConsoleImpl();
-        FlooringMasteryView myView = new FlooringMasteryView(myIo);
-        OrderDao myOrderDao = new OrderDaoFileImpl();
-        ProductDao myProductDao = new ProductDaoFileImpl();
-        TaxDao myTaxDao = new TaxDaoFileImpl();
-        FlooringMasteryServiceLayer myService = new FlooringMasteryServiceLayerImpl(myOrderDao, myProductDao, myTaxDao);
-        FlooringMasteryController controller = new FlooringMasteryController(myView, myService);
+//        UserIO myIo = new UserIOConsoleImpl();
+//        FlooringMasteryView myView = new FlooringMasteryView(myIo);
+//        OrderDao myOrderDao = new OrderDaoFileImpl();
+//        ProductDao myProductDao = new ProductDaoFileImpl();
+//        TaxDao myTaxDao = new TaxDaoFileImpl();
+//        FlooringMasteryServiceLayer myService = new FlooringMasteryServiceLayerImpl(myOrderDao, myProductDao, myTaxDao);
+//        FlooringMasteryController controller = new FlooringMasteryController(myView, myService);
+//        controller.run();
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        FlooringMasteryController controller = ctx.getBean("controller", FlooringMasteryController.class);
         controller.run();
     }
 }
